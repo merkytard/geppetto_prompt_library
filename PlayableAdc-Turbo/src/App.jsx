7b0c1dc6db25488f223e28576d2dba87613abf5f
GeppettoBot <geppetto@system.local>
2025-05-20 01:16:45 +0200

    feat: connect SceneSelector into App layout

--- PlayableAdc-Turbo/src/App.jsx
+++ PlayableAdc-Turbo/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import TimelinePanel from './timeline/TimelinePanel';
+import SceneSelector from './ui/SceneSelector';
 
 export default function App() {
     return (
         <div className="app">
             <h1>PlayableAdc Turbo Editor</h1>
+            <SceneSelector />
             <TimelinePanel />
         </div>
     );
