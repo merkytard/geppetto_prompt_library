08c1096d90097cf0fb4e77462604827aa0304d03
GeppettoBot <geppetto@system.local>
2025-05-20 00:26:11 +0200

    feat: wire TimelinePanel into App layout

---
+++ PlayableAdc-Turbo/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import TimelinePanel from './timeline/TimelinePanel';
+
+export default function App() {
+    return (
+        <div className="app">
+            <h1>PlayableAdc Turbo Editor</h1>
+            <TimelinePanel />
+        </div>
+    );
+}
