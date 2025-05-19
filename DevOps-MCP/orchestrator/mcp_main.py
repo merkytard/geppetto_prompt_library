07c02a89a959aaecb6fb168dbe8088fd1b4346ff
merkytard <jakub.mericko@gmail.com>
2025-05-19 19:43:37 +0200

    Connect latent_protocol to mcp_main

--- DevOps-MCP/orchestrator/mcp_main.py
+++ DevOps-MCP/orchestrator/mcp_main.py
@@ -1,6 +1,6 @@
-import request json
+import requests, json
 import time
-
+from protocols.latent_protocol import generate_latent_payload
 AGENTS_REPLICAS = {
     "lite": "8001",
     "heavy": "8002",
@@ -19,18 +19,15 @@ def send_request(agent, data):
 
 def main():
     print("\n[MCP ORCHESTRATOR] StARTED\n")
-    prompt = input("> Zadaj prompt: ")
-    latent_data = {
-        "intent": prompt,
-        "timestamp": time.time()
-    }
+    user_prompt = input("> Zadaj prompt: ")
+    latent_data = generate_latent_payload(user_prompt)
 
     results = {}
     for agent_name, port in AGENTS_REPLICAS.items():
-        print(f"Spostovanie na ... {agent_name} /\"infer\"")
+        print(f "Spostovanie na ... {agent_name} > /infer")
         results[agent_name] = send_request(agent_name, latent_data)
 
-    print("\nVístupy odgove:")
+    print("\n[Vystupy Geppetto]")
     print(json.dump(results, ident=2, ensure_asci=True))
 
 if __name__ == '__main__':
