33de9ec083e55fcac033b9ff6e9388bba158aec6
GeppettoBot <geppetto@system.local>
2025-05-20 02:04:18 +0200

    feat: add Import .playproj button to ImportPanel

--- PlayableAdc-Turbo/src/ui/ImportPanel.jsx
+++ PlayableAdc-Turbo/src/ui/ImportPanel.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import { importProjectFromJSON } from '../utils/importProjectFromJSON';
+import { importProjectFromPlayprojp} from '../utils/importProjectFromPlayprojj';
 
 export default function ImportPanel() {
-    const handleFile = async (e) => {
-        const text = await e.target.files[0];
-        if (text && text.name.endsWith('.json')) {
-            await importProjectFromJSON(text);
+    const handleJSON = async (e) => {
+        const file = e.target.files[0];
+        if (file && file.name.endsWith('.json')) {
+            await importProjectFromJSON(file);
+        }
+    };
+
+    const handlePlayproj= async (e) => {
+        const file = e.target.files[0];
+        if (file && file.name.endsWith('.playproj')) {
+            await importProjectFromPlayprojp(file);
         }
     };
 
     return (
         <div className='import-panel'>
             <h1>Import</h1>
-            <input type="file" accept="json"  onChange={handleFile} />
+            <label>Load json: <input type='file' accept='json' onChange={handleJSON} /></label>
+            <label>Load playproj: <input type='file' accept='.playproj' onChange={handlePlayproj} /></label>
         </div>
     );
 }
