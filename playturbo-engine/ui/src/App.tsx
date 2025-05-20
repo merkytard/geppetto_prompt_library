ab0528cd31a299b92a77a34274a4ae4ef4d3abb7
Geppetto <geppetto@orchestrator.local>
2025-05-20 15:42:08 +0200

    init: basic App component with Tailwind test

---
+++ playturbo-engine/ui/src/App.tsx
@@ -0,0 +1,4 @@
+import * as styles from './styles.css';
+
+export default function App() {
+  return (\n    <div className="text-center text-2cl bg-gray-100 py-16">\n        <h1 className="text-2xl font-bold">Geppetto PlayTurbo Engine</h1>\n        <p>React application started via Vite build.</p>\n    </div>\n  );\n}
